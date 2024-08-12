a85a6cc72c0f3ca870c723c0c467b296e570ca6b
pyegork <egorkoromyslov@gmail.com>
2024-08-12 23:08:09 +0300

    add_users

---
+++ add_users.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from src.config import client
+from src.models import db
+
+async def check_dialogs():
+    await client.start()
+    async for dialog in client.get_dialogs():
+        if await db.check_user_exists(dialog.chat.id):
+            await db.registrate_user(dialog.chat.id)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(client.run())
+    finally:
+        client.stop()
