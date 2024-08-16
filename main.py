0d4e8d309324e29d7b452cab4bffdcd9807fc0d6
pyegork <egorkoromyslov@gmail.com>
2024-08-16 23:37:45 +0300

    added some logs

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from src.models import db
 from src.utils import Additional
 
 
+@client.on_message(filters.private, group=2)
+async def got_message(_: Client, message: types.Message):
+    logger.debug(f'Second handler got message by [{message.from_user.id}]')
+
+
 @client.on_message(filters.private & ~filters.me & ~filters.bot)
 async def registration_user(_: Client, message: types.Message):
     logger.debug(f'[{message.from_user.id}] sent message')
