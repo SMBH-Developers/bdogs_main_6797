378ae41d167a484e7270c2b3a0f844f633ad36d1
EvaDoubleI <bambyk923@gmail.com>
2024-10-07 21:39:02 +0300

    database transfer

---
+++ get_all_info_db.py
@@ -0,0 +1,54 @@
+import asyncio
+import json
+from typing import Dict
+from datetime import date, datetime
+
+from sqlalchemy import select
+
+from src.models import Card, User, Shift, async_session
+
+
+async def get_users() -> Dict[int, datetime]:  # return {id: datetime}
+    users_dict: Dict = {}
+    async with async_session() as session:
+        users = (await session.execute(select(User))).scalars().all()
+
+    for user in users:
+        users_dict[user.id] = user.registration_date
+
+    return users_dict
+
+
+async def get_shifts() -> Dict[str, date]:  # return {'managers': date}
+    shifts_dict: Dict = {}
+    async with async_session() as session:
+        shifts = (await session.execute(select(Shift))).scalars().all()
+
+    for shift in shifts:
+        shifts_dict[shift.managers] = shift.date
+
+    return shifts_dict
+
+
+async def get_cards() -> Dict[int, str]:  # return {number card: 'status'}
+    cards_dict: Dict = {}
+    async with async_session() as session:
+        cards = (await session.execute(select(Card))).scalars().all()
+
+    for card in cards:
+        cards_dict[card.card] = card.status
+
+    return cards_dict
+
+
+async def write_all_information() -> None:
+    cards = await get_cards()
+    users = await get_users()
+    shifts = await get_shifts()
+
+    result = {'users': users, 'shifts': shifts, 'cards': cards}
+    with open('all_information.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, indent=4)
+
+
+asyncio.run(write_all_information())
---
+++ insert_all_info_db.py
@@ -0,0 +1,43 @@
+import asyncio
+import json
+from typing import Dict
+from datetime import datetime, date
+
+from sqlalchemy import insert
+
+from src.models import User, Card, Shift, async_session
+
+
+async def insert_users(users: Dict[int, datetime]):
+    async with async_session() as session:
+        for user_id, registration_date in users.items():
+            await session.execute(insert(User).values(id=user_id, registration_date=registration_date))
+        await session.commit()
+
+
+async def insert_cards(cards: Dict[int, str]):
+    async with async_session() as session:
+        for card, status in cards.items():
+            await session.execute(insert(Card).values(card=card, status=status))
+        await session.commit()
+
+
+async def insert_shifts(shifts: Dict[str, date]):
+    async with async_session() as session:
+        for managers, date_shift in shifts.items():
+            await session.execute(insert(Shift).values(managers=managers, date=date_shift))
+        await session.commit()
+
+
+async def insert_all_info():
+    with open('all_information.json', 'r', encoding='utf-8') as f:
+        info = json.load(f)
+
+    await insert_users(info['users'])
+    await insert_cards(info['cards'])
+    await insert_shifts(info['shifts'])
+
+    print('Готово')
+
+
+asyncio.run(insert_all_info())
