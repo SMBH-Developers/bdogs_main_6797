45ea512700021a842053be966c2c42d1aa8d6f47
EvaDoubleI <bambyk923@gmail.com>
2024-10-25 20:01:39 +0300

    upd

--- update_users.py
+++ update_users.py
@@ -2,31 +2,42 @@ from datetime import datetime, timedelta, date
 
 from src.config import client
 from src.models import async_session, User
+from src.utils import Additional
 
 from sqlalchemy import update, select
 
 
 async def get_users():
     async with async_session() as session:
-        users = (await session.execute(select(User.id, User.folder).where(User.folder.is_(None)))).scalars().all()
+        users = (await session.execute(select(User.id, User.folder).where(User.registration_date >= datetime.now().replace(hour=00, minute=00, second=00)))).scalars().all()
     return users
-
-
-async def update_user(id_: int):
-    async with async_session() as session:
-        await session.execute(update(User).values(registration_date=User.registration_date - timedelta(days=1)))
-        await session.commit()
 
 
 async def update_users():
     users_with_folder = await get_users()
     count = 0
     users = []
+    folder_counts = {"Нет папки": 0}
+    users_with_folder_dict = {user[0]: user[1] for user in users_with_folder}
     async for dialog in client.get_dialogs():
-        #if dialog.chat.id in users_id:
         messages = [message async for message in client.get_chat_history(dialog.chat.id)]
         if messages[-1].date.date() == date(2024, 10, 25):
-            users.append(dialog.chat.id)
+            user_id = dialog.chat.id
+            users.append(user_id)
             count += 1
+
+            if user_id in users_with_folder_dict:
+                folder = users_with_folder_dict[user_id]
+                if folder in folder_counts:
+                    folder_counts[folder] += 1
+                else:
+                    folder_counts[folder] = 1
+            else:
+                folder_counts["Нет папки"] += 1
+
+    for folder, count in folder_counts.items():
+        print(f"{folder}: {count}")
+
+
 
 
