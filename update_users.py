354e54dcfc8dd4d1da736e9da047c039cacd2753
EvaDoubleI <bambyk923@gmail.com>
2024-10-25 22:23:03 +0300

    upd

--- update_users.py
+++ update_users.py
@@ -1,46 +1,54 @@
-from datetime import datetime, timedelta, date
+import asyncio
+from datetime import datetime, date
+from typing import Dict, List, Tuple
 
 from src.config import client
 from src.models import async_session, User
-from src.utils import Additional
 
-from sqlalchemy import update, select
+from sqlalchemy import select
 
 
 async def get_users():
     async with async_session() as session:
         users = (await session.execute(select(User.id, User.folder).where(User.registration_date >= datetime.now().replace(hour=00, minute=00, second=00)))).all()
-        #print(users)
     return users
 
 
-async def update_users():
-    users_with_folder = await get_users()
-    count = 0
-    users = []
-    folder_counts = {"Нет папки": 0}
-    #print(users_with_folder[1])
+async def get_users_and_dialogs() -> Tuple[Dict[int, str], List[int]]:
+    users_with_folder_task = asyncio.create_task(get_users())
+    dialogs_task = asyncio.create_task(client.get_dialogs(limit=1500))
+
+    users_with_folder, dialogs = await asyncio.gather(users_with_folder_task, dialogs_task)
+
     users_with_folder_dict = {user[0]: user[1] for user in users_with_folder}
-    print(users_with_folder_dict)
-    async for dialog in client.get_dialogs(limit=1500):
-        messages = [message async for message in client.get_chat_history(dialog.chat.id)]
-        if messages[-1].date.date() == date(2024, 10, 25):
-            user_id = dialog.chat.id
-            users.append(user_id)
-            count += 1
-            print(count)
-            if user_id in users_with_folder_dict:
-                folder = users_with_folder_dict[user_id]
-                if folder in folder_counts:
-                    folder_counts[folder] += 1
-                else:
-                    folder_counts[folder] = 1
+
+    relevant_dialogs = [
+        dialog.chat.id
+        for dialog in dialogs
+        if dialog.chat.id in users_with_folder_dict and
+        (await client.get_chat_history(dialog.chat.id))[-1].date.date() == datetime.now().date()
+    ]
+
+    return users_with_folder_dict, relevant_dialogs
+
+
+async def update_users():
+    users_with_folder_dict, relevant_dialogs = await get_users_and_dialogs()
+    folder_counts = {"Нет папки": 0}
+
+    for user_id in relevant_dialogs:
+        if user_id in users_with_folder_dict:
+            folder = users_with_folder_dict[user_id]
+            if folder in folder_counts:
+                folder_counts[folder] += 1
             else:
-                folder_counts["Нет папки"] += 1
+                folder_counts[folder] = 1
+        else:
+            folder_counts["Нет папки"] += 1
 
     for folder, count in folder_counts.items():
         print(f"{folder}: {count}")
-    print(f'Всего: {count}')
+    print(f'Всего: {len(relevant_dialogs)}')
 
 
 
