ddfdb68aa31173fd2a4eab09a6c12711f33f4c66
pyegork <egorkoromyslov@gmail.com>
2024-11-11 15:55:16 +0300

    debug handler test

--- src/tasks/ping/utill.py
+++ src/tasks/ping/utill.py
@@ -41,7 +41,7 @@ async def check_message_read_status(client: Client, message: types.Message) -> O
         sleep_threshold=30
     )
 
-    for dialog in dialogs:
+    for dialog in dialogs.dialogs:
         if message.id < dialog.read_outbox_max_id: # если сообщение прочитано
             return True
 
