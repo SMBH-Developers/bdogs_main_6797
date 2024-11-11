32a5fa53070b6d252683a15518bdbf8ca44a24e0
Keni13-Coder <vladmoldovanov03@gmail.com>
2024-11-11 13:54:54 +0300

    added logs by close_job

--- src/tasks/close_job.py
+++ src/tasks/close_job.py
@@ -13,9 +13,12 @@ def close_job(job):
             job_result = await job(*args, **kwargs)
             if job_result:
                 current_job = scheduler.get_job(job_id, 'default')
+                logger.info(f'Job {current_job} found')
                 if current_job:
                     current_kwargs = current_job.kwargs
+                    logger.info(f'Current kwargs: {current_kwargs}')
                     current_kwargs['message'] = job_result
+                    logger.info(f'Current kwargs after update: {current_kwargs}')
                     
                     scheduler.modify_job(
                         current_job,
@@ -23,6 +26,7 @@ def close_job(job):
                     )
             else:
                 scheduler.remove_job(job_id, 'default')
+                logger.info(f'Job {job_id} removed')
         except Exception as e:
             logger.error(f'Unexpected error closing job {job_id}: {e}')
         
