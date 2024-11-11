b545c194fc99b94f096a3655c293987d9dd27846
Keni13-Coder <vladmoldovanov03@gmail.com>
2024-11-11 11:43:41 +0300

    added extra database param for tests

--- src/models/_engine.py
+++ src/models/_engine.py
@@ -1,6 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
+from sqlalchemy.pool import NullPool
 from ..config import settings
 
-engine = create_async_engine(settings.postgres_dsn.unicode_string())
+if settings.MODE == 'TEST':
+    DATABASE_PARAMS = {'poolclass': NullPool}
+else:
+    DATABASE_PARAMS = {}
+
+engine = create_async_engine(settings.postgres_dsn.unicode_string(), **DATABASE_PARAMS)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
