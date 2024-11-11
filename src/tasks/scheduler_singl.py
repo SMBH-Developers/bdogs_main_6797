e2207bcba473a66d8acc01a2ef6a6b2137bd3861
Keni13-Coder <vladmoldovanov03@gmail.com>
2024-11-11 14:04:35 +0300

    debug test_chain_ping

--- src/tasks/scheduler_singl.py
+++ src/tasks/scheduler_singl.py
@@ -3,14 +3,19 @@ from apscheduler.jobstores.redis import RedisJobStore
 from apscheduler.executors.pool import ProcessPoolExecutor
 from apscheduler.executors.asyncio import AsyncIOExecutor
 from src.config import settings
+from threading import Lock
 
 
 class Singleton:
     _instance = None
+    _lock = Lock()
     
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super().__new__(cls)
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+                    cls._instance._initialized = False
         return cls._instance
     
     
@@ -18,23 +23,25 @@ class Singleton:
 class SchedulerSingleton(Singleton, AsyncIOScheduler):
     
     def __init__(self, **kwargs):
-        jobstores = {
-            'default': RedisJobStore(
-                jobs_key='dispatched_trips_jobs',
-                run_times_key='dispatched_trips_running',
-                host=settings.REDIS_HOST_NAME,
-                db=settings.REDIS_JOB_DATABASES_TEST if settings.MODE == 'TEST' else settings.REDIS_JOB_DATABASES,
-                port=settings.REDIS_PORT,
-                password=settings.REDIS_PASSWORD
+        if not hasattr(self, '_initialized') or not self._initialized:
+            jobstores = {
+                'default': RedisJobStore(
+                    jobs_key='dispatched_trips_jobs',
+                    run_times_key='dispatched_trips_running',
+                    host=settings.REDIS_HOST_NAME,
+                    db=settings.REDIS_JOB_DATABASES_TEST if settings.MODE == 'TEST' else settings.REDIS_JOB_DATABASES,
+                    port=settings.REDIS_PORT,
+                    password=settings.REDIS_PASSWORD
+                )
+            }
+            executors = {
+                'default': AsyncIOExecutor(),
+                'processpool': ProcessPoolExecutor(3)
+            }
+            super().__init__(
+                {'apscheduler.timezone': 'Europe/Moscow'},
+                jobstores=jobstores,
+                executors=executors,
+                **kwargs
             )
-        }
-        executors = {
-            'default': AsyncIOExecutor(),
-            'processpool': ProcessPoolExecutor(3)
-        }
-        super().__init__(
-            {'apscheduler.timezone': 'Europe/Moscow'},
-            jobstores=jobstores,
-            executors=executors,
-            **kwargs
-        )
+            self._initialized = True
