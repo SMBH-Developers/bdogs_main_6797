e8c1b0acb09c006fb44a331b213a95d14ae6cb35
Keni13-Coder <vladmoldovanov03@gmail.com>
2025-01-29 12:39:07 +0300

    debug engine

--- src/database/_engine.py
+++ src/database/_engine.py
@@ -6,7 +6,20 @@ from loguru import logger
 # if settings.MODE == 'TEST':
 #     DATABASE_PARAMS = {'poolclass': NullPool}
 # else:
-DATABASE_PARAMS = {}
+DATABASE_PARAMS = {
+    'pool_pre_ping': True,
+    'pool_size': 5,
+    'max_overflow': 10,
+    'pool_timeout': 30,
+    'pool_recycle': 1800,
+    'echo': False,
+    'connect_args': {
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5
+    }
+}
 
 engine = create_async_engine(settings.postgres_dsn.unicode_string(), **DATABASE_PARAMS)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
